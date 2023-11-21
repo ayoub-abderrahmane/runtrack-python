89b103359a95424509d0a839577a8f177cd738e8
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2023-11-21 10:08:21 +0100

    jour01

--- job04/alphabet.py
+++
@@ -1,6 +0,0 @@
-import string
-
-def listAlphabet():
-    return list(string.ascii_lowercase)
-
-print(listAlphabet())
--- pour allez plus loin/lettre.py
+++
@@ -1,7 +0,0 @@
-x=input ("Entrez la chaine a tester :")
-
-if "e" in x :
-    print("la lettre e est présente")
-else:
-    print("la lettre e est absente")
-
