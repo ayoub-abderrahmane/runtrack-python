5cdf7e066f4acfcd32b9c9d9c81673ef25b858ff
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2023-11-22 10:18:02 +0100

    ajout job03

--- jour03/job02/main.py
+++ jour03/job02/main.py
@@ -2,7 +2,7 @@
 def My_print_name (name):
     print ("Bonjour" , name)
 
-
+# On vient tester le parametre
 My_print_name ("Pierre")
 My_print_name ("Alicia")
 My_print_name ("Celine")
---
+++ jour03/job03/main.py
@@ -0,0 +1,6 @@
+# On vient définir la fonction
+def Add (x, y):
+    print (x + y)
+
+# On vient tester le parametre
+Add(25,25)
