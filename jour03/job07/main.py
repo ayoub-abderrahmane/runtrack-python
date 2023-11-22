b343335166fe523f16a2e0af27c4c26fd58e69d8
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2023-11-22 11:19:12 +0100

    ajout job07

---
+++ jour03/job07/main.py
@@ -0,0 +1,13 @@
+def dev(langage):
+    if langage == "JavaScript":
+        print ("tu es un dévellopeur web")
+    elif langage == "python":
+        print ("tu es un dévellopeur ia")
+    elif langage == "java":
+        print ("tu es un dévellopeur logiciel")
+    elif langage == "reactjs":
+        print ("tu es un dévellopeur mobile")
+    else :
+        print ("un jour,je serai le meilleur développeur")
+
+dev("unjour")
