b485e87cc448e36c6eb7dc2ea2514482e839adf5
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2023-11-22 10:24:46 +0100

    ajout job04

---
+++ jour03/job04/main.py
@@ -0,0 +1,8 @@
+# On vient définir la fonction
+def GetHello():
+    # On attribue la variable x pour la phrase souhaitée
+    x = print("Hello la Plateforme")
+    # On return notre variable x
+    return x
+
+GetHello()
