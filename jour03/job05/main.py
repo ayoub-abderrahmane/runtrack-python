8664e112fcd69dce20f2786581daa641dbf31f75
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2023-11-22 11:04:04 +0100

    ajout job06

--- jour03/job05/main.py
+++ jour03/job05/main.py
@@ -1,4 +1,6 @@
+# on définit la fonction calcule
 def calcule(num1, operator, num2):
+    # on ajoute une boucle qui permet de déterminer le symbole saisi
     if operator == "+":
        print (num1 + num2)
     elif operator == "-":
@@ -10,5 +12,5 @@ def calcule(num1, operator, num2):
     elif operator == "/":
        print (num1 / num2)
 
-    
+# on appelle ensuite la fonction    
 calcule (30, "/", 20)
---
+++ jour03/job06/main.py
@@ -0,0 +1,10 @@
+# On définit la fonction
+def grandeur(nombre):
+    # On créer la boucle charger d'analyser le nombre
+    if nombre >= 0:
+        print ("Positif")
+    elif nombre <= 0:
+        print ("Négatif")
+
+#On appelle la fonction
+grandeur(-15)
